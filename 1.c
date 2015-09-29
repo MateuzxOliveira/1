e94306fcbaddec11a80596ee337dd57e3e56c15a
MateuzxOliveira <teteubb.teu@gmail.com>
2015-09-28 22:07:02 -0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <ctype.h>
 int main(){
   int i, qtd_vogais=0;
   char nome[10];
   printf("\n Nome: ");
-  fgets(nome,10,stdin);
+  fgets(nome,11,stdin);
   for(i=0;i<10;i++){
     if(toupper(nome[i])=='A'||toupper(nome[i])=='E'||toupper(nome[i])=='I'||toupper(nome[i])=='O'||toupper(nome[i])=='U'){
       qtd_vogais++;
@@ -12,3 +13,4 @@ int main(){
   printf("\n Quantidade de vogais: %d\n",qtd_vogais);
   return 0;
 }
+
